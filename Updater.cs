08736309d46cbc74ff40f49e02b48956d3f0d668
Levi <contact@lekuru.xyz>
2024-01-07 04:34:22 +0100

    Initial commit

---
+++ Updater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Updater
+{
+    public partial class Updater : Form
+    {
+        public Updater()
+        {
+            InitializeComponent();
+        }
+    }
+}
